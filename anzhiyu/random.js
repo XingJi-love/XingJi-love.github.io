986934dc7ef1278786827490996771f97198ecec
githubHZB <156395304+githubHZB@users.noreply.github.com>
2024-08-11 22:55:49 +0800

    Site updated: 2024-08-11 22:55:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/16107.html","posts/18950.html"];function toRandomPost(){
+var posts=["posts/16107/","posts/18950/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

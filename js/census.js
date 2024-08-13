d0fc79aeee5aadc4e6ba68a00b803d984a189803
githubHZB <156395304+githubHZB@users.noreply.github.com>
2024-08-13 19:18:49 +0800

    Site updated: 2024-08-13 19:18:47

--- js/census.js
+++
@@ -1,1 +0,0 @@
-<script id="LA-DATA-WIDGET" crossorigin="anonymous" charset="UTF-8" src="https://v6-widget.51.la/v6/3JLyhaDb2n5U14Yn/quote.js?theme=0&col=true&f=12&badge=icon_0&icon=center"></script>

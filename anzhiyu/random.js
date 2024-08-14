ca6f37fd586624baffb8fc5e0240abe6ef6d8119
githubHZB <156395304+githubHZB@users.noreply.github.com>
2024-08-14 18:48:10 +0800

    Site updated: 2024-08-14 18:48:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/64063/","posts/52027/","posts/39261/","posts/9023/","posts/18950/","posts/22439/","posts/57959/","posts/59157/","posts/34027/","posts/2933/","posts/18731/","posts/49708/","posts/26817/","posts/39254/","posts/39958/","posts/6117/","posts/64063/","posts/32969/","posts/62221/"];function toRandomPost(){
+var posts=["posts/52027/","posts/64063/","posts/9023/","posts/39261/","posts/18950/","posts/57959/","posts/22439/","posts/34027/","posts/18731/","posts/2933/","posts/59157/","posts/26817/","posts/49708/","posts/6117/","posts/39254/","posts/39958/","posts/62221/","posts/64063/","posts/32969/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

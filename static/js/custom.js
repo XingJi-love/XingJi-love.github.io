0a115d67f17186aea75acbfafcb35794ad1b0617
XingJi-love <3968523679@qq.com>
2024-11-21 00:38:46 +0800

    Site updated: 2024-11-21 00:38:42

---
+++ static/js/custom.js
@@ -0,0 +1,14 @@
+// 防恶意反代
+const allowedDomains = ['localhost','xingji.fun','love.xingji.fun','cloudflare.xingji.fun','xingji.netlify.app','xingji-love.github.io'];
+
+if (!allowedDomains.includes(document.domain)) {
+    Snackbar.show({
+    text: decodeURI('您现在处于恶意镜像站中,即将跳转回源站!'),
+    pos: 'top-center',
+    actionText: '确定',
+    duration: 5000,
+    onActionClick: () => window.location.href = 'https://love.xingji.fun'
+    });
+
+    setTimeout(() => window.location.href = 'https://love.xingji.fun', 5000);
+}
